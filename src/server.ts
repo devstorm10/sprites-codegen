6d80dda8f1d7f8b4a1ea3e07c32ea974b3b82388
devstorm10 <strongtechman@gmail.com>
2024-09-19 12:47:59 -0400

    Let heroku set port

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,12 @@
 import express, { Request, Response } from 'express';
 
 const app = express();
-const port = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Where are you now that I need you most?');
 });
 
 app.listen(port, () => {
-  console.log(`Server is running at http://localhost:${port}`);
+  console.log(`Server is running at http://localhost:${PORT}`);
 });
